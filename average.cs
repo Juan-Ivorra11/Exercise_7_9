ad6e98d547d8e18e1237e36b5acf997deaad19bf
Juan <juan.ivorra11@gmail.com>
2020-11-19 18:51:50 +0100

    Average clear
    We need to put an introduction with copyright commentary in the top of the file.
    The names of the variables are incorrects because they are "marks" and they are defined by letters, especially _"d" that was the "average", we change it to "mark1, etc...)

--- average.cs
+++ average.cs
@@ -1,20 +1,32 @@
+/*
+ * (c) Juan Ivorra
+ *
+ * Program that asks the user to enter 3 marks and calculates the
+ * average
+ */
+
+
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+public class Average
+{
+    public static void Main()
+    {
+        double mark1 = 0, mark2, mark3 = 0, average;
+        string s;
+        Console.Write("Insert a mark: ");
+        s = Console.ReadLine(); // we read the mark as string to check the word "end"	 
+        while (s != "end")
+        {  // if the mark isn't an end then we pass it to double to work with it
+            mark2 = Convert.ToDouble(s);
+            mark3++;   //counter of marks
+            mark1 += mark2;  // sum of all the notes
+            Console.Write("Insert a mark: ");
+            s = Console.ReadLine();
+        }
+        if (mark3 != 0)
+        {
+            average = mark1 / mark3;  //calculate the average
+            Console.WriteLine("The average is {0}.", average);
+        }
+    }
 }
